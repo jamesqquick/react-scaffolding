c310ebdc1b0814adcceb2255eec87f9cd1f4a074
James Quick <james.q.quick@outlook.com>
2020-07-15 13:28:52 -0500

    Able to scaffold react component

---
+++ out/src/utils/Snippets.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.componentSnippet = void 0;
+exports.componentSnippet = (name) => {
+    return `import React from 'react'
+
+export default function ${name}() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+`;
+};
+//# sourceMappingURL=Snippets.js.map
---
+++ src/utils/Snippets.ts
@@ -0,0 +1,12 @@
+export const componentSnippet = (name) => {
+    return `import React from 'react'
+
+export default function ${name}() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+`;
+};
